dd088018ccbb7d3720d5cad00108d663dd93ba2c
vade <vade@vade.info>
2014-04-03 16:36:17 -0400

    Initial Commit.

---
+++ UltraParty2000VJPro/UltraParty2000VJPro/UP2VJPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UP2VJPAppDelegate.h
+//  UltraParty2000VJPro
+//
+//  Created by vade on 4/3/14.
+//  Copyright (c) 2014 Ultravisual. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UP2VJPAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ UltraParty2000VJPro/UltraParty2000VJPro/UP2VJPAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  UP2VJPAppDelegate.m
+//  UltraParty2000VJPro
+//
+//  Created by vade on 4/3/14.
+//  Copyright (c) 2014 Ultravisual. All rights reserved.
+//
+
+#import "UP2VJPAppDelegate.h"
+
+@implementation UP2VJPAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
